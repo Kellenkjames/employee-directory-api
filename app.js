886a0ab109d98f1054c705f31c24df3d4a08769e
Kellen James <Kellenkjames@gmail.com>
2020-05-29 10:08:07 -0700

    successfully made request to Fetch API (returning 12 results)
    successfully made request to Fetch API (returning 12 results)

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // GLOBAL VARIABLES
 const employees = [];
-const urlAPI = `https:/randomuser.me/api?resuluts=12&inc=name, picture, email, location, phone dob &noinfo & nat=US`;
+const urlAPI = `https://randomuser.me/api?results=12&inc=name, picture, email, location, phone dob &noinfo & nat=US`;
 const gridContainer = document.querySelector('.grid-container');
 const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal-content');
 const modalClose = document.querySelector('.modal-close');
 
+// Fetch Data from the API
+fetch(urlAPI)
+    .then(res => res.json())
+    .then(data => console.log(data));
