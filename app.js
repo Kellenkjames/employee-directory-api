c2b649b8d00605096f99834b6472df24409db688
Kellen James <Kellenkjames@gmail.com>
2020-05-29 12:49:54 -0700

    resposne callback is returning 12 items from API - template literals are injecting data onto the page
    resposne callback is returning 12 items from API - template literals are injecting data onto the page

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // GLOBAL VARIABLES
-const employees = [];
+let employees = [];
 const urlAPI = `https://randomuser.me/api?results=12&inc=name, picture, email, location, phone dob &noinfo & nat=US`;
 const gridContainer = document.querySelector('.grid-container');
 const overlay = document.querySelector('.overlay');
@@ -9,4 +9,39 @@ const modalClose = document.querySelector('.modal-close');
 // Fetch Data from the API
 fetch(urlAPI)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(res => res.results)
+    .then(displayEmployees)
+    .catch(err => console.log(err))
+
+// Display Employees Function
+function displayEmployees(employeeData) {
+    
+    employees = employeeData;
+    
+    
+    // Store the employee HTML as we create it
+    let employeeHTML = '';
+    
+    // Loop through each employee and create HTML markup
+    employees.forEach((employee, index) => {
+
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+        
+        employeeHTML += `
+            <div class="card" data-index="${index}">
+                <img class="card__avatar" src="${picture.large}" />
+                <div class="card__text__container">
+                    <h2 class="card__text__container__name">${name.first} ${name.last}</h2>
+                    <p class="card__text__container__email">${email}</p>
+                    <p class="card__text__container__address">${city}</p>
+                </div>
+            </div>
+        `
+    });
+    
+    gridContainer.innerHTML = employeeHTML;
+    
+}
