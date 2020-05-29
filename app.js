dc77503f4ee7bfd3e75d7595d8e4a09d2dbbfaeb
Kellen James <Kellenkjames@gmail.com>
2020-05-29 12:59:11 -0700

    updated image sizes and changes query for API string to only return US profiles
    updated image sizes and changes query for API string to only return US profiles

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // GLOBAL VARIABLES
 let employees = [];
-const urlAPI = `https://randomuser.me/api?results=12&inc=name, picture, email, location, phone dob &noinfo & nat=US`;
+const urlAPI = `https://randomuser.me/api?results=12&inc=name, picture, email, location, phone dob &noinfo &nat=US`;
 const gridContainer = document.querySelector('.grid-container');
 const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal-content');
