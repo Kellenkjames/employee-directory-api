77f77c1933e2a7678faf545114d8982707c5bd60
Kellen James <Kellenkjames@gmail.com>
2020-05-29 09:57:16 -0700

    setup global variables in app.JS
    setup global variables in app.JS

---
+++ app.js
@@ -0,0 +1,8 @@
+// GLOBAL VARIABLES
+const employees = [];
+const urlAPI = `https:/randomuser.me/api?resuluts=12&inc=name, picture, email, location, phone dob &noinfo & nat=US`;
+const gridContainer = document.querySelector('.grid-container');
+const overlay = document.querySelector('.overlay');
+const modalContainer = document.querySelector('.modal-content');
+const modalClose = document.querySelector('.modal-close');
+
